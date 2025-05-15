ba821b99927a6ac110d55cd9b8fd371d529d6664
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-05-15 16:43:10 +0200

    Lootable buildings

---
+++ 1.5/Source/VanillaQuestsExpanded-Deadlife/VanillaQuestsExpanded-Deadlife/DefOfs/InternalDefOf.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace VanillaQuestsExpandedDeadlife
+{
+	[DefOf]
+	public static class InternalDefOf
+	{
+		static InternalDefOf()
+		{
+			DefOfHelper.EnsureInitializedInCtor(typeof(InternalDefOf));
+		}
+
+		
+
+	}
+}
