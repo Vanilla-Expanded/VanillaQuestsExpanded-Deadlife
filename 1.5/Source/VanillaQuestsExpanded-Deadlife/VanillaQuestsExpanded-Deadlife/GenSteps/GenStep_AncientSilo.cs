f4eb9fb4abae3edbdb8965c773b4ee51c0b017b3
Taranchuk <nfhfyxer@gmail.com>
2025-05-24 18:10:37 +0300

    s

--- 1.5/Source/VanillaQuestsExpanded-Deadlife/VanillaQuestsExpanded-Deadlife/GenSteps/GenStep_AncientSilo.cs
+++ 1.5/Source/VanillaQuestsExpanded-Deadlife/VanillaQuestsExpanded-Deadlife/GenSteps/GenStep_AncientSilo.cs
@@ -10,22 +10,52 @@ namespace VanillaQuestsExpandedDeadlife
         public override void Generate(Map map, GenStepParams parms)
         {
             var center = map.Center;
-            var hatch = ThingMaker.MakeThing(InternalDefOf.VQED_LockedSiloHatch);
-            GenSpawn.Spawn(hatch, center, map, WipeMode.Vanish);
-            var spawnRadius = 10f;
-            
-            var shamblerCount = 5;
-            var lordJob = new LordJob_DefendPoint(center, spawnRadius);
-            var lord = LordMaker.MakeNewLord(Faction.OfEntities, lordJob, map);
+            var clearRadius = 10;
 
-            for (var i = 0; i < shamblerCount; i++)
+            // Clear area around the center
+            foreach (var cell in CellRect.CenteredOn(center, clearRadius))
             {
-                var spawnCell = CellFinder.RandomSpawnCellForPawnNear(center, map, (int)spawnRadius);
-                if (spawnCell.IsValid)
+                if (cell.InBounds(map))
                 {
-                    var pawn = PawnGenerator.GeneratePawn(PawnKindDefOf.ShamblerSwarmer, Faction.OfEntities);
-                    GenSpawn.Spawn(pawn, spawnCell, map);
-                    lord.AddPawn(pawn);
+                    var thingList = cell.GetThingList(map);
+                    for (var i = thingList.Count - 1; i >= 0; i--)
+                    {
+                        var thing = thingList[i];
+                        if (thing.def.mineable || thing.def.IsBuildingArtificial)
+                        {
+                            thing.Destroy();
+                        }
+                    }
+                }
+            }
+
+            // Place the silo hatch
+            var hatchDef = InternalDefOf.VQED_LockedSiloHatch;
+            if (hatchDef != null)
+            {
+                var hatch = ThingMaker.MakeThing(hatchDef);
+                GenSpawn.Spawn(hatch, center, map, WipeMode.Vanish);
+            }
+
+            // Spawn shamblers around the hatch
+            var shamblerDef = PawnKindDefOf.ShamblerSwarmer;
+            if (shamblerDef != null)
+            {
+                var spawnRadius = 10f;
+                var shamblerCount = 5;
+
+                var lordJob = new LordJob_DefendPoint(center, spawnRadius);
+                var lord = LordMaker.MakeNewLord(Faction.OfEntities, lordJob, map);
+
+                for (var i = 0; i < shamblerCount; i++)
+                {
+                    var spawnCell = CellFinder.RandomSpawnCellForPawnNear(center, map, (int)spawnRadius);
+                    if (spawnCell.IsValid)
+                    {
+                        var pawn = PawnGenerator.GeneratePawn(shamblerDef, Faction.OfEntities);
+                        GenSpawn.Spawn(pawn, spawnCell, map);
+                        lord.AddPawn(pawn);
+                    }
                 }
             }
         }
